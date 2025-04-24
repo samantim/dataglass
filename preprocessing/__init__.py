76794ac4b7ae8356e5fb45b984523d5ad4e0917e
samantim <saman.teymouri@gmail.com>
2025-04-25 00:53:14 +0200

    __init__ file completed

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -18,6 +18,21 @@ from .outlier_handling import (
     HandleOutlierMethod
 )
 
+from .type_conversion import (
+    convert_datatype_auto,
+    convert_datatype_ud
+)
+
+from .feature_encoding import (
+    encode_categorical,
+    CategoricalEncodingMethod
+)
+
+from .feature_scaling import (
+    scale_feature,
+    ScalingMethod
+)
+
 __all__ = [
     # missing_handling
     "handle_missing_values_drop",
@@ -33,4 +48,13 @@ __all__ = [
     "handle_outliers",
     "DetectOutlierMethod",
     "HandleOutlierMethod",
+    # type_conversion
+    "convert_datatype_auto",
+    "convert_datatype_ud",
+    # feature_encoding
+    "encode_categorical",
+    "CategoricalEncodingMethod",
+    # feature_scaling
+    "scale_feature",
+    "ScalingMethod"
 ]
